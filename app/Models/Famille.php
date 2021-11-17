81e5d73e87973ef84ab32c762895c004295cdb89
Suylo <spoownz@yahoo.com>
2021-11-17 17:21:29 +0100

    New relationship added

--- app/Models/Famille.php
+++ app/Models/Famille.php
@@ -4,12 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Famille extends Model
 {
 
+    protected $table = 'familles';
     protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
     public $timestamps = false;
 
     use HasFactory;
+
+    /**
+     *
+     * Une famille possède plusieurs médicaments
+     *
+     * @return HasMany
+     */
+    public function medicaments(): HasMany{
+        return $this->hasMany(Medicament::class);
+    }
+
 }
